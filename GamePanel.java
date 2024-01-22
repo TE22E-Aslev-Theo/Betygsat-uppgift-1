c7dbd2abaf9b2164174b55b15e5ae1c1519d8334
TE22E-Aslev-Theo <theo.aslev@elev.ga.ntig.se>
2024-01-22 09:10:03 +0100

    lagt till en uppdatering metod, en målar metod, fixat dimentionerna på skärmen och lagt till så att tråden stanner i 17 millisekunder varje gång i spel tråden

--- GamePanel.java
+++ GamePanel.java
@@ -2,8 +2,9 @@ import javax.swing.JPanel;
 import java.awt.*;
 public class GamePanel extends JPanel implements Runnable{
     
-    final int WIDTH = 900;
+    final int WIDTH = 1066;
     final int HEIGHT = 600;
+
     Thread GameThread;
     GamePanel(){
         Keys keylistener = new Keys();
@@ -18,14 +19,28 @@ public class GamePanel extends JPanel implements Runnable{
         GameThread.start();
     }
 
+    public void paint(Graphics g){
+        super.paint(g);
+
+    }
+    public void update(){
+
+
+    }
 
     @Override
     public void run() {
-
         while (GameThread != null) {
-            
+
+            repaint();
+            update();
+            try {
+                Thread.sleep(17);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         } 
-        
+
     }
 
 }
