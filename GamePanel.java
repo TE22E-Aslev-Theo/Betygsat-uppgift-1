5c0e3ed54eff94516383781190c220179f9705be
TE22E-Aslev-Theo <theo.aslev@elev.ga.ntig.se>
2024-01-22 09:26:32 +0100

    Lagt till gravitation för spelaren

--- GamePanel.java
+++ GamePanel.java
@@ -9,6 +9,8 @@ public class GamePanel extends JPanel implements Runnable{
     public double player_Y;
     final double player_Width = 70;
     final double player_Height = 35;
+    public double gravity = 0.4;
+    public double velocity; 
     Thread GameThread;
     GamePanel(){
         Keys keylistener = new Keys();
@@ -30,8 +32,13 @@ public class GamePanel extends JPanel implements Runnable{
         g2d.drawImage(player,(int)player_X,(int)player_Y,null);
     }
     public void update(){
-
-
+        if (player_Y >= HEIGHT - player_Height) {
+            velocity = 0;
+        } else{
+            player_Y += velocity;
+            velocity += gravity;
+        }
+        
     }
 
     @Override
--- Main.java
+++ Main.java
@@ -4,12 +4,14 @@ public class Main extends JFrame{
     public static void main(String[] args) {
         GamePanel gamepanel = new GamePanel();
         JFrame frame = new JFrame();
+        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("drib yppalf ");
         frame.setResizable(false);
         frame.add(gamepanel);
         frame.pack();
-        frame.setVisible(true);
         gamepanel.StartGame();
+        frame.setVisible(true);
+        
     }
 }
