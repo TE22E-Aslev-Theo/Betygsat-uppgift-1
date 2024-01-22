7d033d82063cd0a51d684c981d2d9d036d90efc1
TE22E-Aslev-Theo <theo.aslev@elev.ga.ntig.se>
2024-01-22 08:52:53 +0100

    lagt till en enkel tangent lyssnare som ska göra så man kan hoppa med space knappen

--- Keys.java
+++ Keys.java
@@ -1,7 +1,32 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Keys{
+public class Keys implements KeyListener{
+
+    public boolean jump; 
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        //används inte 
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        char keycode = e.getKeyChar();
+
+        if (keycode == KeyEvent.VK_SPACE) {
+            jump = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        char keycode = e.getKeyChar();
+
+        if (keycode == KeyEvent.VK_SPACE) {
+            jump = false;
+        }
+    }
 
 
     
